e4576e6cf7cd405d1e0587cafa8934b95d1de3b4
vlad <vladkuzma99@gmail.com>
2019-06-28 22:17:45 +0300

    Inversion Of Control (and Dependency Injection without Spring)

---
+++ src/main/java/com/home/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package com.home;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Classical Melody";
+    }
+}
---
+++ src/main/java/com/home/RockMusic.java
@@ -0,0 +1,8 @@
+package com.home;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Rock Song";
+    }
+}
